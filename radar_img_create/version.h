519189202472569afdf9c096855c86cfaa21b160
Petre Rodan <2b4eda@subdimension.ro>
2023-02-20 08:24:10 +0200

    cleanup

--- radar_img_create/version.h
+++ radar_img_create/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sun Feb 19 21:59:32 UTC 2023
+// compiled on Mon Feb 20 05:23:09 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 25
-#define COMMIT 0
+#define BUILD 26
+#define COMMIT 3
 
 #endif
